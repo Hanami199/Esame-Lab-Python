6ff9fd661291269ea6ebe72fbb1d827dcc453bfc
Sara Trabucco <sara.trabucco99@gmail.com>
2024-06-26 16:35:30 +0200

    Some more tweaks

--- esame.py
+++ esame.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import datetime
 
+
 class ExamException(Exception):
     pass
 
@@ -8,27 +9,14 @@ class ExamException(Exception):
 class CSVTimeSeriesFile:
     def __init__(self, name):
         self.name = name
-        self.can_read = True
-        
-        # Test per verificare che il file esista/sia apribile
-        try:
-            open_file = open(self.name, 'r')
-            open_file.readline()
-            
-        except FileNotFoundError: 
-            self.can_read = False
-            raise ExamException(f'Il file {self.name} non esiste o non è leggibile!')
 
-# get_data prende in input il file CSV e trasforma il contenuto in una lista di liste con ["data", numero di passeggeri] 
+             
+
+    # get_data prende in input il file CSV e trasforma il contenuto in una lista di liste con ["data", numero di passeggeri] 
     def get_data(self):  
         data = [] # lista in cui verranno salvate le liste ["data", n_passengers]
         
-        if not self.can_read:
-            print('C''è stato un errore nell''apertura del file.')
-            return None
-        
-        else: 
-            
+        try:
             with open(self.name, 'r') as file:
                 open_file = csv.reader(file)
                 prev_ts = None
@@ -72,14 +60,18 @@ class CSVTimeSeriesFile:
                     
                     except ValueError:
                         continue # Se il numero di passeggeri non è un intero positivo (o qualsiasi altra cosa), si salta e si continua senza sollevare eccezioni
-                        
+            
+        except FileNotFoundError: 
+            self.can_read = False
+            raise ExamException(f'Il file {self.name} non esiste o non è leggibile!')
+                      
         return data
             
     
 def detect_similar_monthly_variations(time_series, years):
     
     # 1. Gli input devono essere validi:
-    # Si controlla che la lista di anni fornita sia corretta
+    # Si controlla che la lista di anni fornita sia corretta:
     
     if not isinstance(years, list) or len(years) > 2:
         raise ExamException('Deve essere fornita una lista di due anni!')
@@ -91,15 +83,15 @@ def detect_similar_monthly_variations(time_series, years):
     if years[0] != years[1]-1:
         raise ExamException('Devono essere forniti due anni consecutivi!')
     
-    # Si controlla la validità della list:
+    # Si controlla la validità della lista time_series:
     if not isinstance(time_series, list): # deve essere una lista
         raise ExamException('Deve essere fornita una lista di date!') 
     
-    for line in time_series:
+    for line in time_series: # deve contenere liste di due elementi, di cui la prima una stringa e la seconda un intero
         if not isinstance(line, list) or len(line) != 2 or not isinstance(line[0], str) or not isinstance(line[1], int):
             raise ExamException('Il formato delle date ed i valori forniti non sono validi!')
     
-    #2. Si organizzano i dati in modo tale da poter trovare velocemente anno -> mese -> passeggeri
+    # 2. Si organizzano i dati in modo tale da poter trovare velocemente anno -> mese -> passeggeri
     # per tale scopo, si usa un dizionario:
     
     yearly_data = {}
@@ -115,13 +107,12 @@ def detect_similar_monthly_variations(time_series, years):
             if year not in yearly_data:
                 yearly_data[year] = [None] * 12
                 
-            # Si converte il mese in un indice della lista (perché nella lista ci sono 11 elementi come le coppie di mesi) 
-            # e si popola quel mese con i dati a disposizione
+            # Si converte il mese in un indice della lista e si popola quel mese con i dati a disposizione
             yearly_data[year][month-1] = line[1]
     
     for year in years:
         if year not in yearly_data:
-            raise ExamException(f"L'anno richiesto, {year}, non ha dati!")
+            raise ExamException(f"Non ci sono dati a disposizione per l'anno richiesto: {year}!")
 
     # print(yearly_data)
     
@@ -131,16 +122,11 @@ def detect_similar_monthly_variations(time_series, years):
         var2 = None
         
         if yearly_data[years[0]][i+1] is not None and yearly_data[years[0]][i] is not None:
-            # print(yearly_data[years[0]][i+1])
-            # print(yearly_data[years[0]][i])
-
-            var1 = yearly_data[years[0]][i+1] - yearly_data[years[0]][i]
-            
+            var1 = yearly_data[years[0]][i+1] - yearly_data[years[0]][i] 
             # print(f"Differenza tra mese {i} e mese {i+1} è di {var1} nell'anno {years[0]}")
             
         if yearly_data[years[1]][i+1] is not None and yearly_data[years[1]][i] is not None:
             var2 = yearly_data[years[1]][i+1] - yearly_data[years[1]][i]   
-            
             # print(f"Differenza tra mese {i} e mese {i+1} è di {var2} nell'anno {years[1]}")
             
         if var1 is None or var2 is None:
@@ -152,10 +138,3 @@ def detect_similar_monthly_variations(time_series, years):
     return variations
         
         
-# time_series_file = CSVTimeSeriesFile(name='data.csv')
-# time_series = time_series_file.get_data()
-# print(time_series)  
-# print("\n\n")
-# years = [1951, 1952]
-# print(detect_similar_monthly_variations(time_series_file.get_data(), years))
-
